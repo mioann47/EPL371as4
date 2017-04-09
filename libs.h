dac014dc98da6f1bb838766129e078017b877584
cchrys06 <cchrys06@cs.ucy.ac.cy>
2017-04-09 15:15:56 +0300

    libraries

---
+++ libs.h
@@ -0,0 +1,18 @@
+#ifndef __LIBS_H__
+#define __LIBS_H__
+
+#include <sys/types.h> 
+/* For sockets */
+#include <sys/socket.h> 
+/* For sockets */
+#include <netinet/in.h> 
+/* For Internet sockets */
+#include <netdb.h> 
+/* For gethostbyaddr() */
+#include <stdio.h> 
+/* For I/O */
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+#endif
