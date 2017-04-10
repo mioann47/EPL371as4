95b505ba7e49399588df2b943c727711e8405d74
cchrys06 <cchrys06@cs.ucy.ac.cy>
2017-04-10 23:13:52 +0300

    QUEUE added

---
+++ server/queue.h
@@ -0,0 +1,27 @@
+#include "libs.h"
+
+#define TRUE  1
+#define FALSE	0
+
+/* a link in the queue, holds the info and point to the next Node*/
+
+
+typedef struct Node_t {
+    int data;
+    struct Node_t *prev;
+} NODE;
+
+/* the HEAD of the Queue, hold the amount of node's that are in the queue*/
+typedef struct Queue {
+    NODE *head;
+    NODE *tail;
+    int size;
+    int limit;
+} Queue;
+
+Queue *ConstructQueue(int limit);
+void DestructQueue(Queue *queue);
+int Enqueue(Queue *pQueue, NODE *item);
+NODE *Dequeue(Queue *pQueue);
+int isEmpty(Queue* pQueue);
+
