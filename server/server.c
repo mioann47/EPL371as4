df4874b2f91aa76730882e4e76f9a4c5a8815035
mioann47 <mioann47@cs.ucy.ac.cy>
2017-04-10 19:02:21 +0300

    fixed main

--- server/server.c
+++ server/server.c
@@ -12,22 +12,141 @@ const char s[2] = " ";
 // function prototype for reversing func.
 /* Server with Internet stream sockets */
 
-char test[1024];
+int getFileSize(char *path){
+struct stat fstat;
+lstat(path, &fstat);
+return fstat.st_size;
+
+}
+
+void writeIntoSock(int newsock,char *buf,CONFIG *cfg){
+	
+				/** replace **/
+				char *p = buf;
+				while ((*p) != '\0') {
+					if ((*p) == '\r' && (*(p + 1)) == '\n') {
+						(*p) = ' ';
+						(*(p + 1)) = ' ';
+					}
+					p++;
+				}
+
+				/** TOKENIZER **/
+				char *token=NULL;
+				token = strtok(buf, s);
+				char *request=token;
+				
+				char *file = NULL;
+				char *connection = NULL;
+
+
+
+
+
+
+				while (token != NULL) {
+					if (strcmp(token, "GET") == 0       || strcmp(token, "HEAD") == 0 || strcmp(token, "DELETE") == 0) {
+						token = strtok(NULL, s);
+						file = token;
+					}
+
+					if (strcmp(token, "Connection:") == 0) {
+						token = strtok(NULL, s);
+						connection = token;
+					}
+					token = strtok(NULL, s);
+				}
+
+				
+				
+				if (connection==NULL){
+				connection="close";
+				}
+
+				char *msgtemp = NULL;
+
+				if (strcmp(request,"GET") == 0 || strcmp(request, "HEAD") == 0){
+
+				
+
+				
+
+				/*build new path*/
+				char* path= (char*)malloc(strlen(file)+strlen(cfg->server_file_folder)+1);
+				strcpy(path,cfg->server_file_folder);
+				strcat(path,file);
+
+				/*get contents*/
+				char *filebuffer = fileContents(path);
+				int fsize=getFileSize(path);
+
+
+				if (filebuffer == NULL) {
+					msgtemp = msg_not_found();
+					printf("NOT FOUND FILE: %s\n", file);
+				if (write(newsock, msgtemp, strlen(msgtemp)) < 0) {
+					perror("write");
+					exit(1);
+				}
+
+				if (msgtemp != NULL)
+					free(msgtemp);
+				} else {
+						
+					/*get filetype*/
+					char *filetype = NULL;
+					filetype = get_filetype(path);
+
+					/*get header*/
+					msgtemp = msg_ok(filebuffer, connection, filetype,fsize);
+					printf("%s file %s\n",request,file);
+
+				
+
+				
+				/* Send header */	
+				if (write(newsock, msgtemp, strlen(msgtemp)) < 0) {
+					perror("write");
+					exit(1);
+				}
+
+				/* Send contents */
+				if (strcmp(request,"GET") == 0){
+					if (write(newsock, filebuffer, fsize+1) < 0) {
+						perror("write");
+						exit(1);
+					}
+				}
+
+				
+				//free
+				if (msgtemp != NULL)
+					free(msgtemp);
+				if (filetype != NULL)
+					free(filetype);
+				if (filebuffer != NULL)
+					free(filebuffer);
+				if (path!=NULL)
+					free(path);
+				}
+			}else if (strcmp(request,"DELETE") == 0)  {
+			/*delete code*/
+			}else {
+			/*not implemented code*/			
+			}
+
+}
+
+
 int main(int argc, char *argv[]) {
 
 
 	CONFIG *cfg=(CONFIG*)malloc(sizeof(CONFIG));
 	readConfigurations(cfg);
 	
-	char *token;
+	
 
-	sprintf(test, "HTTP/1.1 200 OK\r\n");    //line:netp:servestatic:beginserve
-	sprintf(test, "%sServer: EPL371_SERVER\r\n", test);
-	sprintf(test, "%sContent-length: 211\r\n", test);
-	sprintf(test, "%sConnection: keep-alive\r\n", test);
-	sprintf(test, "%sContent-type: text/html\r\n\r\n", test);
-	sprintf(test, "%s<html><head><title>It worked!!!</title></head><body><h1>Yes, It worked!!!</h1>Click at the image to see a sample text!<br><a href=“sample.txt”><img src=“sample.gif”></a></body></html>",
-			test);
+	
 
 	int newsock;
 	int port, sock, serverlen;
@@ -37,10 +156,7 @@ int main(int argc, char *argv[]) {
 	struct sockaddr *serverptr, *clientptr;
 	struct hostent *rem;
 
-	/*if (argc < 2) {
-		printf("Please give the port number \n");
-		exit(1);
-	}*/
+	
 
 	/* Create socket */
 	if ((sock = socket(PF_INET, SOCK_STREAM, 0)) < 0) {
@@ -99,93 +215,7 @@ int main(int argc, char *argv[]) {
 					exit(1);
 				}
 
-				//printf("%s",buf);
-
-				/** replace **/
-				char *p = buf;
-				while ((*p) != '\0') {
-					if ((*p) == '\r' && (*(p + 1)) == '\n') {
-						(*p) = ' ';
-						(*(p + 1)) = ' ';
-					}
-					p++;
-				}
-
-				/** TOKENIZER **/
-				token = strtok(buf, s);
-				char *file = NULL;
-				char *connection = NULL;
-				while (token != NULL) {
-					if (strcmp(token, "GET") == 0) {
-						token = strtok(NULL, s);
-						file = token;
-					} else if (strcmp(token, "HEAD") == 0) {
-						/*head code */
-					} else if (strcmp(token, "DELETE") == 0) {
-						/*delete code */
-					} else {
-						/* NOT IMPLEMENTED MSG CODE*/
-					}
-
-					if (strcmp(token, "Connection:") == 0) {
-						token = strtok(NULL, s);
-						connection = token;
-					}
-					token = strtok(NULL, s);
-				}
-
-				char *msgtemp = NULL;
-				char *filetype = NULL;
-
-
-				char* path= (char*)malloc(strlen(file)+strlen(cfg->server_file_folder)+1);
-				strcpy(path,cfg->server_file_folder);
-				strcat(path,file);
-				char *filebuffer = fileContents(path);
-
-/*
-			if (file[0] == '/')
-				memmove(file, file + 1, strlen(file));*/
-
-			struct stat fstat;
-			lstat(path, &fstat);
-
-				//printf("FILEDATA = %s\n",filebuffer);
-				if (filebuffer == NULL) {
-					msgtemp = msg_not_found();
-					printf("NOT FOUND FILE: %s\n", path);
-				} else {
-					filetype = get_filetype(path);
-
-					//printf("FILETYPE = %s\n",filetype);
-					msgtemp = msg_ok(filebuffer, connection, filetype,fstat.st_size);
-					printf("Get file %s\n", file);
-
-				}
-
-				printf("MESSAGE = %s\n",msgtemp);
-
-				//if (filebuffer!=NULL){printf("Get file %s with size %d and message size %d\n",file,strlen(filebuffer),strlen(msgtemp));printf("msg = \n%s",filebuffer);}
-				
-				if (write(newsock, msgtemp, strlen(msgtemp)) < 0) {/* Send message */
-					perror("write");
-					exit(1);
-				}
-
-
-				if (filebuffer!=NULL){
-				if (write(newsock, filebuffer, fstat.st_size+1) < 0) {/* Send message */
-					perror("write");
-					exit(1);
-				}}
-
-
-				if (msgtemp != NULL)
-					free(msgtemp);
-				if (filetype != NULL)
-					free(filetype);
-				if (filebuffer != NULL)
-					free(filebuffer);
+				writeIntoSock(newsock,buf,cfg);
 
 			} while (strcmp(buf, "dne") != 0); /*Finish on "end" message*/
 			close(newsock); /* Close socket */
